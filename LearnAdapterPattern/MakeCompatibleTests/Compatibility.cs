c5f00a89fad38e831c8a0abeac731e302a82cd4c
alptuncs <alptuncsever@gmail.com>
2023-09-22 18:30:51 +0300

    add test cases

--- LearnAdapterPattern/MakeCompatibleTests/Compatibility.cs
+++ LearnAdapterPattern/MakeCompatibleTests/Compatibility.cs
@@ -8,6 +8,8 @@ namespace AdapterTests;
 
 public class Compatibility : Spec
 {
+    #region Display & Device
+
     public class XDevice : Device
     {
         public XDevice(IMultimediaPort multimediaPort)
@@ -31,6 +33,39 @@ public class Compatibility : Spec
         display.TryConnect(device, cable).ShouldBeFalse();
     }
 
+    #endregion
+
+
+    #region TV Implementation
+
+    [Test]
+    public void TV_can_connect_to_device_using_compatible_cable()
+    {
+        var tv = GiveMe.ATV();
+        var device = MockMe.ADevice();
+        var cable = MockMe.AMultimediaCable();
+
+        tv.TryConnect(device, cable).ShouldBeTrue();
+    }
+
+    #endregion
+
+    #region Laptop Implementation
+
+    [Test]
+    public void Display_can_connect_to_laptop_using_compatible_cable()
+    {
+        var display = MockMe.ADisplay();
+        var laptop = GiveMe.ALaptop();
+        var cable = MockMe.AMultimediaCable();
+
+        display.TryConnect(laptop, cable).ShouldBeTrue();
+    }
+
+    #endregion
+
+    #region Adapter Implementation
+
     [Test]
     public void TV_can_connect_to_Laptop_using_adapter()
     {
@@ -40,4 +75,6 @@ public class Compatibility : Spec
 
         tV.TryConnect(laptop, adapter).ShouldBeTrue();
     }
+
+    #endregion
 }
